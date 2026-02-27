985531acd8e817d183054f41723ec2234f1c8c2e
pierresouza <pierre.s3@hotmail.com>
2026-02-27 10:44:18 -0300

    fix: add final tags to make nav buttons have the correct style

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,18 +2,41 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
+  const color = "bg-(--button-background)";
+
   return (
     <header className=" text-white flex flex-col items-center justify-center ">
-      <div>
-        <Link href="/" className="flex items-center space-x-2">
-          <Image src="/HeaderImage.png" className="w-full h-full" alt="Logo" width={1920} height={1080} />
+      <div className="w-full">
+        <Link href="/" className="flex w-full justify-center items-center">
+          <Image src="/HeaderImage.png" alt="Logo" width={1920} height={1080} className="w-[80%] h-auto" />
         </Link>
       </div>
-      <div className="flex space-x-4 mt-2">
-        <Link href="/Inscricoes">Inscrições</Link>
-        <Link href="/Editais">Editais</Link>
-        <Link href="/Prestacao">Prestação de Contas </Link>
-        <Link href="/Transparencia">Transparencia</Link>
+      <div className="flex space-x-4 mt-4">
+        <Link
+          className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center "
+          href="/Inscricoes"
+        >
+          Inscrições
+        </Link>
+        <Link
+          className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center "
+          href="/Editais"
+        >
+          Editais
+        </Link>
+        <Link
+          className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center"
+          href="/Prestacao"
+        >
+          <span>Prestação de Contas</span>
+          <span className="text-xl">para subvencionados</span>
+        </Link>
+        <Link
+          className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center "
+          href="/Transparencia"
+        >
+          Transparencia
+        </Link>
       </div>
     </header>
   );
