f13ebf4cfa086f0ba25401f32c98aa0feac21f51
spiroberto <fernandomarquessantos@prefeitura.sp.gov.br>
2026-02-27 12:02:56 -0300

    feat: creating Editais page

--- app/Editais/page.tsx
+++ app/Editais/page.tsx
@@ -1,7 +1,62 @@
+import Link from "next/link";
+
 const Editais = () => {
   return (
-    <div>
+    <div className="text-black">
       <h1>Editais</h1>
+      <section className="flex flex-col ">
+        <Link href="/">Voltar</Link>
+        <p>
+          Confira os editais de credenciamento para os projetos de
+          requalificação edilícia em imóveis localizados no perímetro do
+          Programa Requalifica Centro:
+        </p>
+        <span className="flex flex-col mt-8 mb-4">
+          <b>Chamamento Público nº 01/2025/SMUL</b>
+          <br />
+          <b>Objetivo:</b> Credenciar projetos que promovam intervenções de
+          requalificação edilícia em imóveis localizados no perímetro do
+          Programa Requalifica Centro ao recebimento de subvenção econômica.
+          <br />
+          <a
+            className="text-blue-800 underline cursor-pointer"
+            href="https://capital.sp.gov.br/web/licenciamento/w/acesso_a_informacao/chamamento_publico/chamamento_publico_01-2025"
+          >
+            Saiba mais aqui
+          </a>
+        </span>
+        <hr />
+        <span className="flex flex-col mt-10 mb-4">
+          <b>Chamamento Público nº 02/2024/SMUL</b>
+          <br />
+          <b>Objetivo:</b> Credenciar projetos que promovam intervenções de
+          requalificação edilícia em imóveis localizados no perímetro do
+          Programa Requalifica Centro ao recebimento de subvenção econômica.
+          <br />
+          <a
+            className="text-blue-800 underline cursor-pointer"
+            href="https://capital.sp.gov.br/web/licenciamento/w/acesso_a_informacao/366075"
+          >
+            Saiba mais aqui
+          </a>
+        </span>
+        <hr />
+        <span className="flex flex-col mt-10 ">
+          <b>Chamamento Público nº 01/2023/SMUL</b>
+          <br />
+          <b>Objetivo:</b> Credenciar projetos que promovam intervenções de
+          requalificação edilícia em imóveis localizados no perímetro do
+          Programa Requalifica Centro ao recebimento de subvenção econômica.
+          <br />
+          <a
+            className="text-blue-800 underline cursor-pointer"
+            href="https://capital.sp.gov.br/web/licenciamento/w/acesso_a_informacao/363647"
+          >
+            Saiba mais aqui
+          </a>
+        </span>
+      </section>
+
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-white">Home</h1>
     </div>
   );
 }
