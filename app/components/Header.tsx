c54620ce355d84899f86aeb4ec215c00897d6eb8
pierresouza <pierre.s3@hotmail.com>
2026-02-27 11:00:32 -0300

    chore: define max-width from header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,39 +3,41 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className=" text-white flex flex-col items-center justify-center ">
-      <div className="w-full">
-        <Link href="/" className="flex w-full justify-center items-center">
-          <Image src="/HeaderImage.png" alt="Logo" width={1920} height={1080} className="w-[80%] h-auto" />
-        </Link>
-      </div>
-      <div className="flex space-x-4 mt-4">
-        <Link
-          className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center "
-          href="/Inscricoes"
-        >
-          Inscrições
-        </Link>
-        <Link
-          className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center "
-          href="/Editais"
-        >
-          Editais
-        </Link>
-        <Link
-          className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center"
-          href="/Prestacao"
-        >
-          <span>Prestação de Contas</span>
-          <span className="text-xl">para subvencionados</span>
-        </Link>
-        <Link
-          className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center "
-          href="/Transparencia"
-        >
-          Transparencia
-        </Link>
-      </div>
+    <header className=" text-white flex flex-col items-center justify-center w-full">
+      <nav className="max-w-316.5">
+        <div className="w-full flex justify-center items-center">
+          <Link href="/" className="flex max-w-8xl justify-center items-center">
+            <Image src="/HeaderImage.png" alt="Logo" width={1920} height={1080} className="" />
+          </Link>
+        </div>
+        <div className="flex space-x-6 mt-4">
+          <Link
+            className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center "
+            href="/Inscricoes"
+          >
+            Inscrições
+          </Link>
+          <Link
+            className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center "
+            href="/Editais"
+          >
+            Editais
+          </Link>
+          <Link
+            className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center"
+            href="/Prestacao"
+          >
+            <span>Prestação de Contas</span>
+            <span className="text-xl">para subvencionados</span>
+          </Link>
+          <Link
+            className="p-4 font-bold bg-(--button-background) text-[26px] rounded-sm w-75 text-center hover:underline ease-in-out duration-500 transition-all flex flex-col items-center justify-center "
+            href="/Transparencia"
+          >
+            Transparencia
+          </Link>
+        </div>
+      </nav>
     </header>
   );
 };
