795a4ae6947b2b9d5127973ca04202d1de3eb74f
spiroberto <fernandomarquessantos@prefeitura.sp.gov.br>
2026-03-01 10:17:39 -0300

    refactor(editais): improve structure to be scalable

--- app/Editais/page.tsx
+++ app/Editais/page.tsx
@@ -1,6 +1,9 @@
 import Article from "../components/Article";
 import GoBack from "../components/GoBack";
 import { DM_Sans, Open_Sans } from "next/font/google";
+import { editais } from "../data/editais";
+import { titles } from "../data/titles";
+import { Metadata } from "next";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -14,72 +17,48 @@ const openSans = Open_Sans({
   variable: "--font-open-sans",
 });
 
+export const metadata: Metadata = {
+  title: titles[1].title
+}
+
 const Editais = () => {
   return (
-    <div className="text-black w-316 mx-auto mt-5 pt-11 bg-amber-100">
-      <section className="flex flex-col w-209 mx-auto">
+    <div className="text-black w-325 mx-auto mt-5 pt-11 bg-white">
+      <section className="flex flex-col w-209.5 mx-auto">
         <GoBack></GoBack>
         <div className={`mt-6 ${openSans.className} text-lg`}>
           Confira os editais de credenciamento para os projetos de
           requalificação edilícia em imóveis localizados no perímetro do
           Programa Requalifica Centro:
         </div>
-        <div
-          className={`flex flex-col mt-8 mb-4 ${openSans.className} text-lg`}
-        >
-          <b className={`${dmSans.className} text-lg`}>
-            Chamamento Público nº 01/2025/SMUL
-          </b>
-          <p>
-            <strong>Objetivo:</strong> Credenciar projetos que promovam
-            intervenções de requalificação edilícia em imóveis localizados no
-            perímetro do Programa Requalifica Centro ao recebimento de subvenção
-            econômica.
-          </p>
-          <a
-            className="text-blue-800 underline"
-            href="https://capital.sp.gov.br/web/licenciamento/w/acesso_a_informacao/chamamento_publico/chamamento_publico_01-2025"
-          >
-            Saiba mais aqui
-          </a>
-        </div>
-        <hr />
-        <div
-          className={`flex flex-col mt-10 mb-4 ${openSans.className} text-lg`}
-        >
-          <b className={`${dmSans.className} text-lg`}>
-            Chamamento Público nº 02/2024/SMUL
-          </b>
-          <p>
-            <b>Objetivo:</b> Credenciar projetos que promovam intervenções de
-            requalificação edilícia em imóveis localizados no perímetro do
-            Programa Requalifica Centro ao recebimento de subvenção econômica.
-          </p>
-          <a
-            className="text-blue-800 underline"
-            href="https://capital.sp.gov.br/web/licenciamento/w/acesso_a_informacao/366075"
-          >
-            Saiba mais aqui
-          </a>
-        </div>
-        <hr />
-        <div className={`flex flex-col mt-10 ${openSans.className} text-lg`}>
-          <b className={`${dmSans.className} text-lg`}>
-            Chamamento Público nº 01/2023/SMUL
-          </b>
-          <p>
-            <b>Objetivo:</b> Credenciar projetos que promovam intervenções de
-            requalificação edilícia em imóveis localizados no perímetro do
-            Programa Requalifica Centro ao recebimento de subvenção econômica.
-          </p>
-          <a
-            className="text-blue-800 underline"
-            href="https://capital.sp.gov.br/web/licenciamento/w/acesso_a_informacao/363647"
-          >
-            Saiba mais aqui
-          </a>
-        </div>
-        <Article/>
+        {editais.map((edital, index) => (
+          <div key={edital.id}>
+            <div className={`flex flex-col mt-8 ${openSans.className} text-lg`}>
+              <b className={`${dmSans.className} text-lg`}>
+                Chamamento Público nº {edital.numero}
+              </b>
+
+              <p>
+                <b>Objetivo:</b> Credenciar projetos que promovam intervenções
+                de requalificação edilícia em imóveis localizados no perímetro
+                do Programa Requalifica Centro ao recebimento de subvenção
+                econômica.
+              </p>
+
+              <a
+                className="text-blue-800 underline"
+                href={edital.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Saiba mais aqui
+              </a>
+            </div>
+
+            {index !== editais.length - 1 && <hr className="mt-6" />}
+          </div>
+        ))}
+        <Article />
       </section>
     </div>
   );
