e944823da5a04843c3031beb7c0131f348040efa
spiroberto <fernandomarquessantos@prefeitura.sp.gov.br>
2026-03-01 10:15:38 -0300

    chore: add mock data for development

---
+++ app/data/titles.ts
@@ -0,0 +1,18 @@
+export const titles = [
+  {
+    id: 1,
+    title: "Inscrições | Subvenção para requalificação de imóveis no centro",
+  },
+  {
+    id: 2,
+    title: "Editais | Subvenção para requalificação de imóveis no centro",
+  },
+  {
+    id: 3,
+    title: "Prestação de Contas | Subvenção para requalificação de imóveis no centro",
+  },
+  {
+    id: 4,
+    title: "Transparência | Subvenção para requalificação de imóveis no centro",
+  }
+];
