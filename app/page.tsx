b0e2e5d516491e9011802753ab4a43714f131d37
pierresouza <pierre.s3@hotmail.com>
2026-03-05 14:14:10 -0300

    feat: create new component to have text in diferent positios in desktop and mobile

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,44 @@ import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarInse
 import { Skeleton } from "@/components/ui/skeleton";
 import { ChevronLeft } from "lucide-react";
 
+export function InfoMap() {
+  return (
+    <div className="flex flex-col gap-3 p-4">
+      {/* Título com entrelinha justa (leading-tight) */}
+      <p className="text-base font-bold text-[#0a3297] leading-tight">Subvenção Econômica para o Centro: incentivo à requalificação de imóveis e ao adensamento populacional</p>
+
+      {/* Parágrafos com entrelinha confortável mas compacta (leading-snug) */}
+      <p className="text-base font-normal text-black leading-snug tracking-tight">
+        A Prefeitura está investindo na transformação do centro de São Paulo. Por meio do Programa de Subvenção Econômica, imóveis da região podem receber incentivo financeiro para obras de retrofit —
+        com foco especial na criação de moradias de interesse social.
+      </p>
+
+      <p className="text-base font-normal text-black leading-snug tracking-tight">
+        Prevista no Plano Urbanístico do Centro (Lei nº 17.844/2022) e regulamentada pelo Decreto nº 62.878/2023, a iniciativa promove chamamentos públicos para selecionar empreendimentos e viabilizar
+        a requalificação de edifícios subutilizados.
+      </p>
+
+      <p className="text-base font-normal text-black leading-snug tracking-tight">
+        Aqui, você encontra todas as informações sobre os empreendimentos contemplados, as regras do programa e a prestação de contas das ações realizadas.
+      </p>
+    </div>
+  );
+}
+
 export default function Home() {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
     <main className="flex justify-center mt-4">
-      <div className="flex w-full max-w-325 bg-white overflow-hidden h-185 rounded">
+      <div className="flex max-md:flex-col  w-full max-w-325 bg-white overflow-hidden h-185 rounded">
+        <div className="block md:hidden">
+          <InfoMap />
+        </div>
         <SidebarProvider open={isOpen} onOpenChange={setIsOpen} className="w-full h-full min-h-full">
           <Sidebar
             collapsible="none"
             className={`
-              w-[20rem] border-r bg-white transition-all ease-in-out
+              w-[20rem] border-r bg-white transition-all ease-in-out hidden md:block
               duration-700 
               ${isOpen ? "ml-0" : "-ml-[20rem]"}
             `}
@@ -24,24 +51,8 @@ export default function Home() {
             <SidebarContent className="min-w-[19rem]">
               <SidebarGroup>
                 {/* gap-3 aproxima os blocos de texto */}
-                <SidebarGroupContent className="flex flex-col gap-3 p-4">
-                  {/* Título com entrelinha justa (leading-tight) */}
-                  <p className="text-base font-bold text-[#0a3297] leading-tight">Subvenção Econômica para o Centro: incentivo à requalificação de imóveis e ao adensamento populacional</p>
-
-                  {/* Parágrafos com entrelinha confortável mas compacta (leading-snug) */}
-                  <p className="text-base font-normal text-black leading-snug tracking-tight">
-                    A Prefeitura está investindo na transformação do centro de São Paulo. Por meio do Programa de Subvenção Econômica, imóveis da região podem receber incentivo financeiro para obras
-                    de retrofit — com foco especial na criação de moradias de interesse social.
-                  </p>
-
-                  <p className="text-base font-normal text-black leading-snug tracking-tight">
-                    Prevista no Plano Urbanístico do Centro (Lei nº 17.844/2022) e regulamentada pelo Decreto nº 62.878/2023, a iniciativa promove chamamentos públicos para selecionar empreendimentos
-                    e viabilizar a requalificação de edifícios subutilizados.
-                  </p>
-
-                  <p className="text-base font-normal text-black leading-snug tracking-tight">
-                    Aqui, você encontra todas as informações sobre os empreendimentos contemplados, as regras do programa e a prestação de contas das ações realizadas.
-                  </p>
+                <SidebarGroupContent className="p-0">
+                  <InfoMap />
                 </SidebarGroupContent>
               </SidebarGroup>
             </SidebarContent>
@@ -50,7 +61,7 @@ export default function Home() {
           <SidebarInset className="relative p-0 m-0 bg-transparent flex-1 overflow-hidden">
             <SidebarTrigger
               onClick={() => setIsOpen(!isOpen)}
-              className="w-10 h-14 absolute z-50 top-0 left-0 bg-[#a6bbf5] rounded-l-none text-white hover:text-black cursor-pointer shadow-md transition-all duration-300"
+              className="w-10 h-14 absolute z-50 top-0 left-0 bg-[#a6bbf5] rounded-l-none text-white hover:text-black cursor-pointer shadow-md transition-all duration-300 hidden md:flex"
             >
               {/* O ícone também gira suavemente na mesma velocidade da sidebar (duration-700) */}
               <ChevronLeft className={`transition-transform duration-700 ${!isOpen ? "rotate-180" : ""}`} />
